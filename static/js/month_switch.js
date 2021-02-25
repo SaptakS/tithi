f8a75589196debbadbf55f4bf96ca06cb71653d4
Saptak S <saptak013@gmail.com>
2021-02-25 21:38:20 +0530

    Improve blur states for dropdown and events

--- static/js/month_switch.js
+++ static/js/month_switch.js
@@ -10,16 +10,21 @@ function show_month_label(month_year) {
 
 function show_month_cal(month_year) {
   const month_cal_val = month_year.dataset["month"];
-  console.log(month_cal_val)
   clear_months();
   show_month_label(month_year);
   document.getElementById(month_cal_val).style.display = "block";
 }
 
 document.querySelectorAll('.month-year').forEach(function(month_year) {
-  console.log(month_year);
   month_year.addEventListener('click', function(e) {
     e.preventDefault();
-    show_month_cal(month_year)
+    show_month_cal(month_year);
+    e.target.blur();
   });
+});
+
+document.querySelectorAll('.event-summary').forEach(function(event_summary) {
+  event_summary.addEventListener('blur', function(e) {
+    e.target.parentElement.removeAttribute("open");
+  })
 });
